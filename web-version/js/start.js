d2bffeff7050ea1f4299930c1ba658c08bcb980a
736242160 <71439514+736242160@users.noreply.github.com>
2026-04-29 10:24:11 +0800

    Add files via upload

--- web-version/js/start.js
+++ web-version/js/start.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function validateSession() {
     // 跳转到登录页面
-    showToast('欢迎使用医语小助手');
+    showToast('欢迎使用见微知心');
     setTimeout(() => {
         redirectTo('login.html');
     }, 1500);
